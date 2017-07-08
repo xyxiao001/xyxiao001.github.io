10cf5bb324a54ffddaff39c3c1708b44e0abeeb8
xyxiao001 <463618622@qq.com>
2017-07-08 21:58:02 +0800

    改版

---
+++ src/Index.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="container">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
